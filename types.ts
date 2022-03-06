9c4d4cbc7ab033df06a8d21d2e1a5a39f46484be
Volodymyr Kryzhanovskyi <vkryzhanovskyi@care.com>
2022-03-06 15:05:20 +0200

    words array

--- types.ts
+++ types.ts
@@ -2,7 +2,9 @@ const strr: string = "Hello anal"
 
 const numTimes: number[] = [5, 10, 20 , 1000]
 const numTimesArr: Array<number> = [7, 9, 4 , 444]
+const wordsArray: Array<string> = ["ptn", "hello"]
 
 console.log(strr)
 console.log(numTimes)
 console.log(numTimesArr)
+console.log(wordsArray  )
