762c84cf5d517e9ae89527b4c4239bd036b54a9e
vavas <vkryzhanovskyi@care.com>
2022-03-06 13:35:11 +0200

    practicing with basic types

---
+++ types.ts
@@ -0,0 +1,8 @@
+const strr: string = "Hello anal"
+
+const numTimes: number[] = [5, 10, 20 , 1000]
+const numTimesArr: Array<number> = [7, 9, 4 , 444]
+
+console.log(strr)
+console.log(numTimes)
+console.log(numTimesArr)
